1a2a7f505f1147ab16ade877b37cab014426fa59
David Mazza <github@dmzza.com>
2013-02-14 13:15:23 -0800

    Minor fix: caption is shown if no tags are found

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -2,7 +2,7 @@ Vine.ApplicationController = Ember.Controller.extend({
   currentPathChanged: function() {
     var page;
 
-    // Track the page in Google Analytics
+    // Thanks: http://www.pansapien.com/2013/01/27/using-google-analytics-with-ember-js/
     if (!Ember.isNone(_gaq)) {
       // Assume that if there is a hash component to the url then we are using
       // the hash location strategy. Otherwise, we'll assume the history
@@ -11,7 +11,6 @@ Vine.ApplicationController = Ember.Controller.extend({
              window.location.hash.substring(1) :
              window.location.pathname;
 
-      // You need your Google Analytics code already loaded for _ga to be initialized
       _gaq.push(['_trackPageview', page]);
     }
   }.observes('currentPath')

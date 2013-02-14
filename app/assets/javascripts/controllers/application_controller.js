e6afce2abe1089ddfca54153ee4a3c35b1d8c40c
David Mazza <github@dmzza.com>
2013-02-14 11:19:24 -0800

    Google Analytics pageview tracking code

--- app/assets/javascripts/controllers/application_controller.js
+++ app/assets/javascripts/controllers/application_controller.js
@@ -1,3 +1,18 @@
 Vine.ApplicationController = Ember.Controller.extend({
+  currentPathChanged: function() {
+    var page;
 
+    // Track the page in Google Analytics
+    if (!Ember.isNone(_gaq)) {
+      // Assume that if there is a hash component to the url then we are using
+      // the hash location strategy. Otherwise, we'll assume the history
+      // strategy.
+      page = window.location.hash.length > 0 ?
+             window.location.hash.substring(1) :
+             window.location.pathname;
+
+      // You need your Google Analytics code already loaded for _ga to be initialized
+      _gaq.push(['_trackPageview', page]);
+    }
+  }.observes('currentPath')
 });
